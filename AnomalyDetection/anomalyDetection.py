56f8c17682075c29628ea7892f40603fb3fd7559
Justinas Globys <52034687+Globys031@users.noreply.github.com>
2022-05-03 22:29:09 +0300

    Improved anomaly detection

--- AnomalyDetection/anomalyDetection.py
+++ AnomalyDetection/anomalyDetection.py
@@ -1,19 +1,52 @@
 from sklearn.datasets import make_blobs
-from numpy import quantile, random, where
 from sklearn.ensemble import IsolationForest
+from sklearn.decomposition import PCA
+
+from numpy import quantile, random, where
+
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+class AnomalyDetector:
+  def __init__(self, learning_data):
+    self.learning_data = learning_data
+
+  def printInput(self):
+    print(self.learning_data)
+
+  # Currently there's no contamination as there's no attack data examples. But you can't have
+  # contamination = 0. So for now 0.01 will suffice.
+  # TO DO: gather (or generate yourself) attack data. We'll later use the algorithm to check if
+  # that data appears as anomalies
+  def findAnomalies(self):
+    # We're going to use all columns for anomaly detection
+    # The only column that might not be of importance is "time", but for now we'll leave it as is.
+    # contamination - percentage of outlier points in the data
+    model=IsolationForest(n_estimators=100, max_samples='auto', contamination=0.01,
+                          max_features=1.0, bootstrap=False, n_jobs=-1, random_state=42, verbose=0)
+    # Fits the models and makes predictions on whether there's outliers
+    model.fit(self.learning_data.values)
+    self.learning_data['anomaly'] = model.predict(self.learning_data.values)
+    outliers = self.learning_data.loc[self.learning_data['anomaly'] == -1]
+    # Gets the indexes of data that are anomalies
+    outlier_indexes = list(outliers.index)
+
+    return outlier_indexes
 
 
-def drawAnomalyGraph(X, _):
-  # https://machinelearningmastery.com/anomaly-detection-with-isolation-forest-and-kernel-density-estimation/
-  plt.scatter(X[:, 0], X[:, 1], marker="o", c=_, s=25, edgecolor="k")
+  def drawAnomalies(self, outlier_indexes):
+    # PCA is primarily used for dimensionality reduction (basically allows to display multiple columns in 2D)
+    pca = PCA(2)
+    pca.fit(self.learning_data.values)
 
-  IF = IsolationForest(n_estimators=100, contamination=.03)
-  predictions = IF.fit_predict(X)
+    # It seems to be calculated differently every time
+    transformed_data = pd.DataFrame(pca.transform(self.learning_data.values))
 
-  # -1 is for outliers and 1 for regular data 
-  outlier_index = where(predictions==-1)
-  values = X[outlier_index]
-  plt.scatter(X[:,0], X[:,1])
-  plt.scatter(values[:,0], values[:,1], color='y')
-  plt.show()
+    plt.title("IsolationForest")
+
+    b1 = plt.scatter(transformed_data[0], transformed_data[1], c='green', s=20,label="normal points")
+    b1 = plt.scatter(transformed_data.iloc[outlier_indexes, 0], transformed_data.iloc[outlier_indexes, 1], c='red',s=20,label="predicted outliers")
+    
+    plt.legend(loc="upper right")
+    plt.show()
--- DataGenerator/dataGenerator.py
+++ DataGenerator/dataGenerator.py
@@ -1,33 +1,51 @@
 import random
 from datetime import datetime, timezone, timedelta
 import datetime as dt
+import pandas as pd
 
-year = 2022
-month = 2
-day = 15
-zone = 3
+def randomData():
+    year = 2022
+    month = 2
+    day = 15
+    zone = 3
 
-bytes = [246,210]
-LOC = [9321,8138,8193]
+    bytes = [246,210]
+    LAC = [9321,8138,8193]
 
-# generate random time of the day
-hour = 1
-previousDate= -1
-while hour < 24:
-    min = 0
-    while min < 59:
-        sec = 0
-        while sec < 59:
-            date = dt.datetime(year, month, day, hour, min, sec,tzinfo=timezone(timedelta(hours=zone)))
+    df = pd.DataFrame()
+    # Initialize empty dataframe
+    for column in [ "time", "bytes", "last_location", "travel_distance", "LAC" ]:
+        df[column] = ""
 
-            if previousDate == -1:
-                dateDiff = 0
-            else:
-                dateDiff = date - previousDate   
+    # generate random time of the day
+    hour = 1
+    previousDate = -1
 
-            distance=random.randint(1,14)    
-            print(str(date.strftime("%Y-%m-%dT%H:%M:%S%z"))+";"+str(random.choice(bytes))+";"+str(dateDiff)+";"+str(distance)+";"+str(random.choice(LOC)))
-            previousDate = date
-            sec = random.randint(sec+1,60)
-        min = random.randint(min+1,60)
-    hour = random.randint(hour+1,24)
+    while hour < 24:
+        min = 0
+        while min < 59:
+            sec = 0
+            while sec < 59:
+                date = dt.datetime(year, month, day, hour, min, sec,tzinfo=timezone(timedelta(hours=zone)))
+
+                if previousDate == -1:
+                    dateDiff = date - date
+                else:
+                    dateDiff = date - previousDate   
+
+                distance=random.randint(1,14)
+
+                # If possible we'll group data by the minute (look at "time" column).
+                df = df.append({'time' : date.minute + hour * 60, 
+                                'bytes' : random.choice(bytes), 
+                                'last_location' : int(dateDiff.total_seconds()),  
+                                'travel_distance' : distance,  
+                                'LAC' : random.choice(LAC), }, ignore_index = True)
+
+                #print(date.strftime("%Y-%m-%dT%H:%M:%S%z"))+";"+str(random.choice(bytes))+";"+str(dateDiff)+";"+str(distance)+";"+str(random.choice(LAC)))
+                previousDate = date
+                sec = random.randint(sec+1,60)
+            min = random.randint(min+1,60)
+        hour = random.randint(hour+1,24)
+
+    return df
