80aed4f2eba73228b250eaa2e2b5d0dbf722986c
Paulius <pauliusgil@gmail.com>
2022-05-09 00:14:06 +0300

    Pass parameters to dateGenerator method

--- DataGenerator/dataGenerator.py
+++ DataGenerator/dataGenerator.py
@@ -3,24 +3,18 @@ from datetime import datetime, timezone, timedelta
 import datetime as dt
 import pandas as pd
 
-def randomData():
-    startYear = 2022
-    startMonth = 2
-    startDay = 15
-    zone = 3
 
-    numOfDaysToGenerateData = 5
-    minDifferenceBetweenDays = 1
-    maxnDifferenceBetweenDays = 5
+def randomData(startYear=2022, startMonth=2, startDay=15, zone=3,
+               numOfDaysToGenerateData=5, minDifferenceBetweenDays=1, maxDifferenceBetweenDays=5):
 
     date = dt.date(startYear, startMonth, startDay)
 
-    bytes = [246,210]
-    LAC = [9321,8138,8193]
+    bytes = [246, 210]
+    LAC = [9321, 8138, 8193]
 
     df = pd.DataFrame()
     # Initialize empty dataframe
-    for column in [ "load_date", "time", "bytes", "last_location", "travel_distance", "LAC" ]:
+    for column in ["load_date", "time", "bytes", "last_location", "travel_distance", "LAC"]:
         df[column] = ""
 
     # generate random time of the day
@@ -42,24 +36,26 @@ def randomData():
                     if previousDate == -1:
                         dateDiff = 0
                     else:
-                        dateDiff = int((fullDate - previousDate).total_seconds())   
-                    
-                    distance=random.randint(1,14)
+                        dateDiff = int(
+                            (fullDate - previousDate).total_seconds())
+
+                    distance = random.randint(1, 14)
 
                     # If possible we'll group data by the minute (look at "time" column).
-                    df = pd.concat([df, pd.DataFrame.from_records([{'load_date' : date,
-                                    'time' : min + hour * 60, 
-                                    'bytes' : random.choice(bytes), 
-                                    'last_location' : dateDiff,  
-                                    'travel_distance' : distance,  
-                                    'LAC' : random.choice(LAC), }])], ignore_index = True)
+                    df = pd.concat([df, pd.DataFrame.from_records([{'load_date': date,
+                                    'time': min + hour * 60,
+                                                                    'bytes': random.choice(bytes),
+                                                                    'last_location': dateDiff,
+                                                                    'travel_distance': distance,
+                                                                    'LAC': random.choice(LAC), }])], ignore_index=True)
 
-                    #print(str(fullDate.strftime("%Y-%m-%dT%H:%M:%S%z"))+";"+str(random.choice(bytes)
-                                                                              # )+";"+str(dateDiff)+";"+str(distance)+";"+str(random.choice(LOC)))
+                    # print(str(fullDate.strftime("%Y-%m-%dT%H:%M:%S%z"))+";"+str(random.choice(bytes)
+                    # )+";"+str(dateDiff)+";"+str(distance)+";"+str(random.choice(LOC)))
                     previousDate = fullDate
-                    sec = random.randint(sec+1,60)
-                min = random.randint(min+1,60)
-            hour = random.randint(hour+1,24)
-        date = date + timedelta(days=random.randint(minDifferenceBetweenDays, maxnDifferenceBetweenDays))       
+                    sec = random.randint(sec+1, 60)
+                min = random.randint(min+1, 60)
+            hour = random.randint(hour+1, 24)
+        date = date + \
+            timedelta(days=random.randint(
+                minDifferenceBetweenDays, maxDifferenceBetweenDays))
     return df
-
