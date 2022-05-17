6b36fa6c9bf188298cd4b231edbf42958577621f
Justinas Globys <52034687+Globys031@users.noreply.github.com>
2022-05-17 19:00:48 +0300

    Fixed some bugs. Started working on live mode

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Requires python 3.0 and up, and multiple modules installed:
 ```
-pip install nbformat numpy pandas matplotlib sklearn warnings
+pip install nbformat numpy pandas matplotlib sklearn warnings dash
 ```
 
 ### How data is portrayed in the table:
@@ -10,5 +10,9 @@ pip install nbformat numpy pandas matplotlib sklearn warnings
 - gray - not an anomaly
 - yellow - a small anomaly (can be safely ignored)
 - red - anomaly
+- % change shows how much the data in the row above differs from the data in the row below
 
 As for the graph, both the yellow (insignifcant) and the red anomalies are portrayed as red
+
+**Note** that the matplotlib graph shows outliers based on all values. The separate tables
+shows further in detail each attribute values that's considered an anomaly
--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from sklearn.datasets import make_blobs
 from numpy import quantile, random, where
 from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
+import dash
 #####
 
 #####
 # # Additional files
-from AnomalyDetection import anomalyDetection
+import AnomalyDetection.anomalyDetection as anomalyDetection
 from DataGenerator import dataGenerator
 #####
 
@@ -22,17 +23,24 @@ from DataGenerator import dataGenerator
 #####
 # # Main code
 
+# app = dash.Dash(__name__)
+
 df = dataGenerator.randomData(startYear=2022, startMonth=5, startDay=10, zone=3,
                numOfDaysToGenerateData=10, minDifferenceBetweenDays=1, maxDifferenceBetweenDays=5)
 
 anomalyDetector = anomalyDetection.AnomalyDetector(df)
 
+# anomalyDetector = anomalyDetection.AnomalyDetector(df, app)
+# anomalyDetector.plotAnomaliesLive()
+
 # # Draws a graph of anomalies for all columns
-# TO DO: get rid of the error when these two are uncommented
-#anomaly_indexes = anomalyDetector.getAnomalyIndexes()
-#anomalyDetector.drawAnomaliesAllColumns(anomaly_indexes)
+anomaly_indexes = anomalyDetector.getAnomalyIndexes()
+anomalyDetector.drawAnomaliesAllColumns(anomaly_indexes)
 
 # Goes further in as to why each data point is an anomaly
 anomalyDetector.identifyAnomalies()
 
 ######
+
+# if __name__ == '__main__':
+#   app.run_server(host="127.0.0.1", port=8080, debug=True)
